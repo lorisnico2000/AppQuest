8a0638f42be37b22605c5295bed2f3475dc4c951
Loris Leuenberger <loris.leuenberger@bict.ch>
2017-12-01 17:00:57 +0100

    Updates

--- app/src/main/java/ch/appquest/bict/appquest/Schatzkarte.java
+++ app/src/main/java/ch/appquest/bict/appquest/Schatzkarte.java
@@ -1,19 +1,27 @@
 package ch.appquest.bict.appquest;
 
+import android.*;
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,102 +36,93 @@ import static android.content.Context.LOCATION_SERVICE;
  * Created by loris on 13.10.2017.
  */
 
-public class Schatzkarte extends Fragment implements LocationListener {
+public class Schatzkarte extends Fragment {
 
     View myView;
-    MapView map;
-    IMapController controller;
 
-    private TextView latituteField;
-    private TextView longitudeField;
+    private TextView txtCoordinates;
+    private Button btnLocation;
     private LocationManager locationManager;
-    private String provider;
+    private LocationListener locationListener;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         myView = inflater.inflate(R.layout.schatzkarte_layout, container, false);
         Context ctx = getActivity().getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-/*
-        map = (MapView) myView.findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setMaxZoomLevel(20);
 
-        map.setMultiTouchControls(true);
-        map.setBuiltInZoomControls(true);
+        txtCoordinates = (TextView) myView.findViewById(R.id.txtCoordinates);
+        btnLocation = (Button) myView.findViewById(R.id.btnLocation);
 
-        controller = map.getController();
-        controller.setZoom(18);
-        LocationManager service = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
-        boolean enabled = service
-                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                txtCoordinates.append("\n " + location.getLatitude() + " " + location.getLongitude());
+            }
 
-// check if enabled and if not send user to the GSP settings
-// Better solution would be to display a dialog and suggesting to
-// go to the settings
-        if (!enabled) {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-        }*/
-        latituteField = (TextView) myView.findViewById(R.id.TextView02);
-        longitudeField = (TextView) myView.findViewById(R.id.TextView04);
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
-        // Get the location manager
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        // Define the criteria how to select the locatioin provider -> use
-        // default
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider( criteria, true );
-        Location location = locationManager.getLastKnownLocation(provider);
+            }
 
-        // Initialize the location fields
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
-        } else {
-            latituteField.setText("Location not available");
-            longitudeField.setText("Location not available");
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET}, 10);
+            }
+        }else{
+            configureButton();
         }
+        configureButton();
+
         return myView;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+        }
+    }
+
+    void configureButton(){
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager.requestLocationUpdates("gps", 2000, 3, locationListener);
+                Toast.makeText(getActivity(), "Button pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /* Request updates at startup */
     @Override
     public void onResume() {
         super.onResume();
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
     }
 
     /* Remove the locationlistener updates when Activity is paused */
     @Override
     public void onPause() {
         super.onPause();
-        locationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        int lat = (int) (location.getLatitude());
-        int lng = (int) (location.getLongitude());
-        latituteField.setText(String.valueOf(lat));
-        longitudeField.setText(String.valueOf(lng));
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        //Toast.makeText(this, "Enabled new provider " + provider, Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        //Toast.makeText(this, "Disabled provider " + provider, Toast.LENGTH_SHORT).show();
     }
 }
